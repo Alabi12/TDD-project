02fd4826950b0c1ace525da683c882ccf64a8ed4
Alabi12 <wolo22001@gmail.com>
2023-02-16 14:06:46 +0000

    Add a test file to test the class Solver with TDD resulting 1 error occured outside of example

--- solver.rb
+++ solver.rb
@@ -1,20 +1,20 @@
-class Solver
-  def factorial(value)
-    raise ArgumentError, 'N must be 0 or positive integer.' if value.negative?
-    return 1 if value.zero?
+# class Solver
+#   def factorial(value)
+#     raise ArgumentError, 'N must be 0 or positive integer.' if value.negative?
+#     return 1 if value.zero?
 
-    (1..value).reduce(:*)
-  end
+#     (1..value).reduce(:*)
+#   end
 
-  def reverse(word)
-    word.reverse
-  end
+#   def reverse(word)
+#     word.reverse
+#   end
 
-  def fizzbuzz(number)
-    return 'fizzbuzz' if (number % 15).zero?
-    return 'fizz' if (number % 3).zero?
-    return 'buzz' if (number % 5).zero?
+#   def fizzbuzz(number)
+#     return 'fizzbuzz' if (number % 15).zero?
+#     return 'fizz' if (number % 3).zero?
+#     return 'buzz' if (number % 5).zero?
 
-    number.to_s
-  end
-end
+#     number.to_s
+#   end
+# end
