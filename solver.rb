2564920a93f03a847306c4789ce96465e6a40f32
Alabi12 <wolo22001@gmail.com>
2023-02-16 14:17:07 +0000

    Add the method fizzbuzz with number as parameter in the solver class

--- solver.rb
+++ solver.rb
@@ -9,13 +9,12 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 15).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
-
-#   def fizzbuzz(number)
-#     return 'fizzbuzz' if (number % 15).zero?
-#     return 'fizz' if (number % 3).zero?
-#     return 'buzz' if (number % 5).zero?
-
-#     number.to_s
-#   end
-# end
