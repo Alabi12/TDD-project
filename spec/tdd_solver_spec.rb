926e1189338e228be29e21bb9b647f7d424564d2
Alabi12 <wolo22001@gmail.com>
2023-02-16 14:15:28 +0000

    Add the method fizzbuzz TDD test which failed to pass

--- spec/tdd_solver_spec.rb
+++ spec/tdd_solver_spec.rb
@@ -13,23 +13,22 @@ describe Solver do
       expect(@solver.factorial(5)).to eql 120
     end
   end
+
+  context 'Testing the reverse method' do
+    it 'It should return olleh if given hello' do
+      expect(@solver.reverse('hello')).to eql 'olleh'
+    end
+  end
+
+  context 'Testing the fizzbuzz method' do
+    it 'should return a string fizz if divible by 3' do
+      expect(@solver.fizzbuzz(6)).to eql 'fizz'
+    end
+    it 'should return a string buzz if divible by 5' do
+      expect(@solver.fizzbuzz(10)).to eql 'buzz'
+    end
+    it 'should return a string fizzbuzz if divible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+  end
 end
-
-#   context 'Testing the reverse method' do
-#     it 'It should return olleh if given hello' do
-#       expect(@solver.reverse('hello')).to eql 'olleh'
-#     end
-#   end
-
-#   context 'Testing the fizzbuzz method' do
-#     it 'should return a string fizz if divible by 3' do
-#       expect(@solver.fizzbuzz(6)).to eql 'fizz'
-#     end
-#     it 'should return a string buzz if divible by 5' do
-#       expect(@solver.fizzbuzz(10)).to eql 'buzz'
-#     end
-#     it 'should return a string fizzbuzz if divible by 3 and 5' do
-#       expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
-#     end
-#   end
-# end
